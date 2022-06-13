571a84741890d04373ce6b9c06782fd85234e8c6
Denis Dashkevich <majorprestige99@gmail.com>
2022-06-13 19:22:35 +0300

    complete header section

--- src/js/menu.js
+++ src/js/menu.js
@@ -7,7 +7,7 @@
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-active');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');
