7d86091c8ce1969c69d5a34b8347985adb731946
Denis Dashkevich <majorprestige99@gmail.com>
2022-06-14 16:40:38 +0300

    complete modal form and header section

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,6 +3,7 @@
   const mobileMenuRef = document.querySelector('[data-menu]');
   const bodyuRef = document.querySelector('body');
   const logo = document.querySelector('[data-logo]');
+  const menuItem = document.querySelectorAll('.nav__link');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
@@ -14,5 +15,13 @@
     mobileMenuRef.classList.toggle('is-open');
     bodyuRef.classList.toggle('no-scroll');
     logo.classList.toggle('menu-logo');
+  });
+  menuItem.forEach(link => {
+    link.addEventListener('click', () => {
+      bodyuRef.classList.toggle('no-scroll');
+      mobileMenuRef.classList.toggle('is-open');
+      menuBtnRef.classList.toggle('is-active');
+      logo.classList.toggle('menu-logo');
+    });
   });
 })();
