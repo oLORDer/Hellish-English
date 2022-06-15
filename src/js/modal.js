c69a348aa230aace8286cbd430dbda64d12f00d4
Sergey Gulaga <ry6ens@gmail.com>
2022-06-15 16:23:18 +0300

    add anchor changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,22 +1,3 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelectorAll('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//     mobileMenuRef: document.querySelector('[data-menu]'),
-//   };
-
-//   refs.openModalBtn.forEach(btn => btn.addEventListener('click', toggleModal));
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     document.body.classList.toggle('modal-open');
-//     refs.modal.classList.toggle('is-hidden');
-//     refs.mobileMenuRef.classList.remove('is-open');
-//     refs.mobileMenuRef.classList.toggle('is-hidden');
-//   }
-// })();
-
 (() => {
   const refs = {
     openModalBtn: document.querySelectorAll('[data-modal-open]'),
@@ -47,13 +28,10 @@
 const btnAbout = document.querySelector('[data-about-open]'),
   modalAbout = document.querySelector('[data-about-menu]');
 
-
 btnAbout.addEventListener('click', toggleAbout);
 modalAbout.addEventListener('click', toggleAbout);
-
 
 function toggleAbout() {
   document.body.classList.toggle('no-scroll');
   modalAbout.classList.toggle('is-hidden');
-
 }
