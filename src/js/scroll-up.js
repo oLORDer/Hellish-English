ee8d64e2bd5a6e84dc6b82a296c53a19bc727fd4
zerkel1991 <acorus1991@gmail.com>
2022-06-15 14:38:01 +0300

    footer-and-scrollup

---
+++ src/js/scroll-up.js
@@ -0,0 +1,12 @@
+const scrollBtn = document.querySelector('.isShowBtn');
+window.onscroll = () => {
+    if(window.scrollY > 700){
+        scrollBtn.classList.remove('isShowBtn_hide');
+    }
+     else if(window.scrollY < 700){
+        scrollBtn.classList.add('isShowBtn_hide');
+    }
+};
+scrollBtn.onlclick = () => {
+    window.scrollTo(0, 0);
+}; 
