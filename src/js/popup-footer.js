b79c593708074e2e7eb2677387dc8e74867a9423
Andrii <andryushka137@gmail.com>
2022-06-16 22:03:21 +0300

    footer-form

--- src/js/popup-footer.js
+++ src/js/popup-footer.js
@@ -1,3 +1,6 @@
+const formName = document.getElementById('user-name');
+const formPhone = document.getElementById('user-phone');
+
 // переменная формы в футере
 let footer = document.getElementById('footer-form'),
   // переменная ящик успешного отзыва
@@ -5,24 +8,16 @@ let footer = document.getElementById('footer-form'),
   // кнопка сабмит
   popupToggle = document.getElementById('footer-submit-button');
 function switchModule() {
-    footer.classList.toggle('is-shown');
-    popup.classList.toggle('is-hidden');
+  footer.classList.toggle('is-shown');
+  popup.classList.toggle('is-hidden');
 }
-
 
 popupToggle.addEventListener('click', event => {
   event.preventDefault();
+  formPhone.value = 'done';
+  formName.value = 'done';
+  formPhone.style.borderColor = 'green';
+  formName.style.borderColor = 'green';
   switchModule();
-  setTimeout(switchModule,3000);
-  // setInterval(5000);
+  setTimeout(switchModule, 3000);
 });
-
-
-
-// popupToggle.addEventListener('click', event => {
-//     event.preventDefault();
-//     footer.classList.toggle('is-shown');
-//     popup.classList.toggle('is-hidden');
-//     setTimeout(1000);
-//     // setInterval(5000);
-//   });
